852ec5a6b7e967e66f8b554a1065d383f3e8a1a5
macGrIS <m.a.cooper@bristol.ac.uk>
2016-01-08 14:21:28 +0000

    changed syntax on spin-globe.py

--- src/spin_globe.py
+++ src/spin_globe.py
@@ -1,8 +1,4 @@
-
-# coding: utf-8
-
-# In[21]:
-
+# import modules
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -10,18 +6,11 @@ import matplotlib.cm as cm
 from mpl_toolkits.basemap import Basemap, shiftgrid
 from matplotlib.backends import backend_agg as agg # raster backend
 from pylab import rcParams
-
-
-# In[22]:
-
 from __future__ import print_function
 from ipywidgets import interact, interactive, fixed
 import ipywidgets as widgets # interactive controls in browser
 get_ipython().magic('matplotlib inline')
 rcParams['figure.figsize'] = 8,8
-
-
-# In[23]:
 
 # Dummy data
 tweets_time= np.arange('2015-12-31T00Z', '2016-01-02T00Z', 30, dtype='datetime64[s]') #YYMMDD T00z == midnight, [s] is seconds
@@ -29,11 +18,6 @@ tweets = np.random.rand(len(tweets_time),2)
 tweets_lon = tweets[:,0]*180-90
 tweets_lat = tweets[:,0]*360-180
 tweets=np.array([tweets_lat,tweets_lon,tweets_time]).transpose() #transpose data to get correct format
-
-tweets
-
-
-# In[24]:
 
 # Spinning globe code
 
@@ -43,9 +27,7 @@ t_lat = data[:,0]
 t_lon = data[:,1]
 time = data[:,2]
 
-
-# In[26]:
-
+# map
 def globe_tweet(rot=0,time=0):
     m = Basemap(projection='ortho', lon_0=rot, lat_0=20, resolution='c')
     m.shadedrelief(scale=0.1)
@@ -63,18 +45,5 @@ def globe_tweet(rot=0,time=0):
     m.plot(x,y,'bo',markersize=9)
     plt.show()
 
-
-# In[27]:
-
 interact(globe_tweet,rot=(-180,180,45),time=(0,1000,1))
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
 
