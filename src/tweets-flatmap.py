4e2f9fbb96c0dcbaa78951afb624a7415428069b
macGrIS <m.a.cooper@bristol.ac.uk>
2016-01-08 14:11:29 +0000

    better tweet-flatmap.py comments

--- src/tweets-flatmap.py
+++ src/tweets-flatmap.py
@@ -1,7 +1,5 @@
-
-# coding: utf-8
-
-# In[80]:
+# by macGrIS
+# import modules
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,55 +7,17 @@ get_ipython().magic('matplotlib inline')
 from __future__ import print_function
 from ipywidgets import interact, interactive, fixed
 import ipywidgets as widgets # interactive controls in browser
-
-
-# In[81]:
-
-tweets_time = np.arange('2015-12-31T00Z', '2016-01-02T00Z', 30, dtype='datetime64[s]') # YYMMDD T00Z == midnight, [s] is seconds
-
-
-# In[82]:
-
-tweets = np.random.rand(len(tweets_time),2)
-
-
-# In[83]:
-
-tweets_lon = tweets[:,0]*180-90
-
-
-# In[84]:
-
-tweets_lat = tweets[:,1]*360-180
-
-
-# In[85]:
-
-tweets=np.array([tweets_lon,tweets_lat,tweets_time]).transpose() # transpose to get it in correct orientation
-
-
-# In[105]:
-
-tweets[:,0]
-
-
-# In[86]:
-
-plt.plot(tweets_lon,tweets_lat,'ro',markersize=1.8)
-
-
-# In[87]:
-
-len(tweets)
-
-
-# In[88]:
-
 from mpl_toolkits.basemap import Basemap
 
+# set up some dummy data
+tweets_time = np.arange('2015-12-31T00Z', '2016-01-02T00Z', 30, dtype='datetime64[s]') # YYMMDD T00Z == midnight, [s] is seconds
+tweets = np.random.rand(len(tweets_time),2)
+tweets_lon = tweets[:,0]*180-90
+tweets_lat = tweets[:,1]*360-180
+# merge data
+tweets=np.array([tweets_lon,tweets_lat,tweets_time]).transpose() # transpose to get it in correct orientation
 
-# In[113]:
-
+# define plotting functon
 def tweet_merc(i=0):
     my_map=Basemap(projection='merc', resolution='c',
                   lat_0=0,lon_0=0,
@@ -82,19 +42,7 @@ def tweet_merc(i=0):
                     
     plt.show()
 
-
-# In[114]:
-
+# interact
 tweets_len = len(tweets_lon)
 interact(tweet_merc,i=(0,tweets_len,1))
-
-
-# In[93]:
-
-tweets_lon[1]
-
-
-# In[ ]:
-
-
 
